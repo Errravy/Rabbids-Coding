8d876bda1af45f6525acf02c3879898b320501e0
Dikta <gucopradiktya72@gmail.com>
2023-03-20 10:43:36 +0700

    Add Button

---
+++ Button.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "InteractableObject.h"
+#include "Button.h"
+
+using namespace std;
+
+
+namespace RabbidsCoding
+{
+    void Button:: interact()
+    {
+        cout << "Button Hit" << endl;
+    }
+}
