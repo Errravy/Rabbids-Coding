5cd17a42b312522a7970a5b973db8b96be9287ab
udaypenangsang <udaxdiraizel@gmail.com>
2023-05-16 15:30:14 +0700

    ArmedRabbids

--- src/Objects/ControllableObject/ArmedRabbids/ArmedRabbids.hpp
+++ src/Objects/ControllableObject/ArmedRabbids/ArmedRabbids.hpp
@@ -1,11 +1,41 @@
 #pragma once
 #include "../ControllableObject.hpp"
-
+#include "../../../Enums/Commands.hpp"
+#include "../../../Enums/Directions.hpp"
+#include "../../../Command/ICommand.hpp"
+#include <iostream>
+#include <unordered_map>
+#include <utility>
 class ArmedRabbids : public ControllableObject
 {
-private: int direction;
+private:
+    std::string objectSymbol = "A";
+    std::unordered_map<Commands, ICommand*> _Command;
+    int _x;
+    int _y;
+    Directions _z;
+    bool _isSucked;
+
 public:
-    int getDirection() override;
-    void setDirection(int newDirection) override;
-    void Interact() ;
+    ArmedRabbids();
+
+    ICommand* setCommand(ICommand* command);
+
+    ICommand* getCommand(Commands commands);
+
+    void setPosition(int x, int y);
+
+    void setPositionX(int x);
+
+    void setPositionY(int y);
+
+    std::pair<int, int> getPosition();
+
+    void setDirection(int direction);
+
+    Directions getDirection();
+
+    bool isSucked();
+
+    std::string getObjectSymbol();
 };
