e8d83de955d0866c08591ffeda12db2d1502b354
jasiin <tiowema.jastin2@gmail.com>
2023-05-16 21:58:49 +0700

    malah error coy

--- src/Playspace/Level/Level.hpp
+++ src/Playspace/Level/Level.hpp
@@ -1,8 +1,26 @@
+#ifndef LEVEL_H
+#define LEVEL_H
+
+#include "../Grid/Grid.h"
+#include "../../Command/Invoker/Invoker.h"
+#include "../../Objects/Controlable/IControlable.h"
+
 class Level
 {
 private:
-    /* data */
+    Grid grid;
+    Invoker invoker;
+    IControlable* controlable;
+    bool isCompleted;
+
 public:
-    Level(/* args */) {}
-    ~Level() {}
+    Level(Invoker invoker);
+    Grid& GetGrid();
+    Invoker& GetInvoker();
+    void ShowAvailableMoves();
+    bool IsCompleted();
+    void SetControlable(IControlable* controlable);
+    IControlable* GetControlable();
 };
+
+#endif // LEVEL_H
