e1fa656dc81a25909f011862e847603e40f4ad80
jasiin <tiowema.jastin2@gmail.com>
2023-05-17 09:14:21 +0700

    akau jawa (update playsapce)

--- src/Playspace/Grid/Grid.hpp
+++ src/Playspace/Grid/Grid.hpp
@@ -1,37 +1,31 @@
-#pragma once
-
 #ifndef GRID_H
 #define GRID_H
 
 #include <iostream>
 #include <string>
-#include <unordered_map>
-
+#include <map>
 #include "../Cell/Cell.hpp"
-#include "../../Objects/IObjects.hpp" // Assuming the header file for IObjects is included
+#include "../../Objects/IObjects.hpp"
 
-class Grid
-{
+class Grid {
 private:
-    int _temp = 0;
-    int _width;
-    int _height;
-    std::string grid[10][10];
-    std::unordered_map<std::pair<int, int>, Cell *> cells; // Assuming CellHash is a hash function for Cell's key
+    int width;
+    int height;
+    std::string** grid;
+    std::map<std::pair<int, int>, Cell*> cells;
 
-    Cell *previousCell;
-    IObjects *previousObject;
-
-    void initializeGridAndCell(int, int);
-    void generateGrid();
+    Cell* previousCell;
+    IObjects* previousObject;
 
 public:
-    Grid(int, int);
-    void checkCell(IObjects *);
-    void checkGridPos(int, int);
-    int getWidth();
-    int getHeight();
-    std::unordered_map<std::pair<int, int>, Cell *> &getCells();
+    Grid(int width, int height);
+    void InitializeGridAndCell(int width, int height);
+    void GenerateGrid();
+    void CheckCell(IObjects* obj);
+    void CheckGridPos(int x, int y);
+    int GetWidth();
+    int GetHeight();
+    std::map<std::pair<int, int>, Cell*> GetCells();
 };
 
-#endif
+#endif // GRID_H
