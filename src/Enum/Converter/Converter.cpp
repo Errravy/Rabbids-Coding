3c81e0e7f09cdaadf80bec41cc8b45f504fd5ee1
Muhammad Khalish Zhafran <khalish260303@gmail.com>
2023-06-01 10:50:01 +0700

    Update Converter.cpp

--- src/Enum/Converter/Converter.cpp
+++ src/Enum/Converter/Converter.cpp
@@ -3,9 +3,27 @@
 Objects Converter::convertStringToObjects(const std::string &objectType)
 {
     static const std::unordered_map<std::string, Objects> objectMap{
-        {"Rabbids", Objects::Rabbids},
         {"Button", Objects::Button},
-        {"Bag", Objects::Bag}};
+        {"ElectricFence", Objects::ElectricFence},
+        {"Rabbids", Objects::Rabbids},
+        {"IdleRabbids", Objects::IdleRabbids},
+        {"WashingMachine", Objects::WashingMachine},
+        {"VacuumRobot", Objects::VacuumRobot},
+        {"ArmedRabbids", Objects::ArmedRabbids},
+        {"ClawRobot", Objects::ClawRobot},
+        {"Plunger", Objects::Plunger},
+        {"Bomb", Objects::Bomb},
+        {"Bag", Objects::Bag},
+        {"Bubble", Objects::Bubble},
+        {"Soap", Objects::Soap},
+        {"CardBox", Objects::CardBox},
+        {"Ring", Objects::Ring},
+        {"Sausage", Objects::Sausage},
+        {"Saw", Objects::Saw},
+        {"Tissue", Objects::Tissue},
+        {"ToiletPaper", Objects::ToiletPaper},
+        {"WateringCan", Objects::WateringCan},
+    };
 
     auto it = objectMap.find(objectType);
     if (it != objectMap.end())
